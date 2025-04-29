97f9ef7ee349139605922da8de89ddd85c05192e
AlifAkbar1645 <134115049+alif1645@users.noreply.github.com>
2025-04-29 13:42:30 +0800

    Menghapus data firebase yang terekspos sebelumnya dari repository

--- config/firebase.js
+++ config/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCpBWrW0I_XRgm92maxDTrTKnE-vfT9lnc",
-  authDomain: "automated-air-purifier-project.firebaseapp.com",
-  projectId: "automated-air-purifier-project",
-  storageBucket: "automated-air-purifier-project.firebasestorage.app",
-  messagingSenderId: "767161415365",
-  appId: "1:767161415365:web:2a8ed4926ee80e5f85a2ac",
-  measurementId: "G-KY2FT1BGRY"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
